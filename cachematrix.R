1a7b7489ec7dbd83b6c4d2b863652ec251910862
gcondeh <gcondeh@gmail.com>
2023-06-20 17:20:10 +0200

    Ajustamos comentarios

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,29 @@
 # getwd()
 # rm(list=ls())
 
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-## Esta funciÛn crear una serie de funciones que almacenan los objetos en el entorno
-## En realidad es un constructor de funciones que se invocar·n desde la resoluciÛn de la cache
+## Esta funci√≥n crear una serie de funciones que almacenan los objetos en el entorno
+## En realidad es un constructor de funciones que se invocar√°n desde la resoluci√≥n de la cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #Incializamos el valor
   inversa <- NULL
   
+  # Asingamos valor a la matriz, dejando en blanco la inversa
   set <- function(matriz) {
     x <<- matriz
     inversa <<- null
   }
   
+  # Funciones para dar valores y recuperar valores de la inversa
   set_inversa <- function (inv)  inversa <<- inv
   get_matriz <- function() x
   get_inversa <- function() inversa
-  
+
+  # Devolvemos una lista de funciones
   list(set = set, set_inversa=set_inversa, get_matriz=get_matriz, get_inversa=get_inversa)
 }
 
-
-## Write a short comment describing this function
-## Resuelve la inversa. Si est· en Cache la captura, si no est· en cache la genera
-
+## Resuelve la inversa. Si est√° en Cache la captura, si no est√° en cache la genera
 
 cacheSolve <- function(x, ...) {
   inv <- x$get_inversa()
@@ -50,7 +45,6 @@ cacheSolve <- function(x, ...) {
 # Creamos la matriz
 A <- matrix(c(1,4,9,0,-3,2,2,7,8),3,3)
 B <- matrix(c(22,12,19,71,5,-4,23,26,0),3)
-
 x<-1000
 c <- matrix (sample(x^2),x)
 
@@ -60,13 +54,13 @@ M2 <- makeCacheMatrix(B)
 M3 <- makeCacheMatrix(c)
 
 M1$get_matriz()  ## Nos da la matriz
-M1$get_inversa() ## Hasta que no generamos la cachÈ deberÌa ser nulo
+M1$get_inversa() ## Hasta que no generamos la cach√© deber√≠a ser nulo
 
 cacheSolve(M1) -> I
 cacheSolve(M2) -> I
 
-# Prueba
+# Prueba de la traspuesta
 B %*% I
 
-
+# Para comprobar la mejora de rendimiento, en la creaci√≥n de la matriz vs la recuperaci√≥n desde la cache
 system.time(cacheSolve(M3))
